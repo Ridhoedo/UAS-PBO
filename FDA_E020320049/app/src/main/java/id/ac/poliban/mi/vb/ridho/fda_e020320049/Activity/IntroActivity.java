9fd737ad420adf3e66fea9a9e4a37ffd532f80e7
Ridhooedo <ridhomuhammad2257@gmail.com>
2023-01-15 14:13:01 +0800

    TASK III

---
+++ FDA_E020320049/app/src/main/java/id/ac/poliban/mi/vb/ridho/fda_e020320049/Activity/IntroActivity.java
@@ -0,0 +1,30 @@
+package id.ac.poliban.mi.vb.ridho.fda_e020320049.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import id.ac.poliban.mi.vb.ridho.fda_e020320049.R;
+
+public class IntroActivity extends AppCompatActivity {
+
+    private ConstraintLayout startBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        setTitle("Ridho - FDA_E020320049");
+
+        startBtn=findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(IntroActivity.this,MainActivity.class));
+            }
+        });
+    }
+}
---
+++ FDA_E020320049/app/src/main/java/id/ac/poliban/mi/vb/ridho/fda_e020320049/Interface/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package id.ac.poliban.mi.vb.ridho.fda_e020320049.Interface;
+
+public interface ChangeNumberItemsListener {
+    void changed();
+}
